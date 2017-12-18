d4d9f29929867153196468309c15d338c4b57b34
Bogdan Babych <bogdan.babych@gmail.com>
2017-12-18 16:01:06 +0000

    s002 creating 1st module

---
+++ src/p1020k1010readText/p1020k1010m1010readText.py
@@ -0,0 +1,17 @@
+'''
+Created on 18 Dec 2017
+
+@author: bogdan
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
