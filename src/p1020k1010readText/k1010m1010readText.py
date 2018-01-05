2e5accb0dc4ca3d32c27c0fa8a746981a0eb83dc
Bogdan Babych <bogdan.babych@gmail.com>
2018-01-05 14:55:40 +0000

    s000x development update

--- src/p1020k1010readText/k1010m1010readText.py
+++ src/p1020k1010readText/k1010m1010readText.py
@@ -8,34 +8,64 @@ import pathlib
 import xml.etree.ElementTree as ET
 
 
+class clGenerateOutputFileNames(object):
+	'''
+	service file naming class:
+	generates output file names, similar to pathlib function for input file names
+	'''
+
+	def __init__(self, SFNameTemplate = './clReadTMXInput.txt', LSuffixes=['out']):
+		self.LFileNamesOut = []
+		self.genFileNamesOut(SFNameTemplate, LSuffixes) # modifies self.LFileNamesOut
+		return
+	
+	def getData(self):
+		return self.LFileNamesOut
+	
+
+	def genFileNamesOut(self, SFNameTemplate, LSuffixes):
+		'''
+		using SFNameTemplate (normally - input file name) as a template for generating a list of output file names
+		'''
+		SHead, Tail = os.path.split(SFNameTemplate) # splitting directory name from file+extension name
+		SRoot, SExtension = os.path.splitext(Tail) # splitting extension from the Tail (file+extension)
+		
+		self.LFileNamesOut = [ SRoot + '-' + SSuffix + 's01' + SExtension for SSuffix in LSuffixes ]
+		SLFileNamesOut = str(self.LFileNamesOut)
+		# SRootOut = SRoot + 's01' + SExtension
+		# SFileNameOut = os.path.join(SHead, SRootOut)
+		
+		print ("%(SHead)s %(Tail)s %(SRoot)s %(SExtension)s %(SFileNameOut)s\n" % locals())
+		print ("%(SLFileNamesOut)s" % locals())
+		return
+		
+
 class clReadTMX(object):
 	'''
+	main class
 	readsTMX file / string and outputs one-line per-sentence output 
 	'''
 	def __init__(self, STmxIn):
 		'''
 		Constructor
 		'''
-
-		# print("output\n", STmxIn)
-		self.getFileOut(sys.argv[1])
+		print("output from clReadTmx\n", STmxIn)
 		return
 	
-	def getFileOut(self, SInputFileName = './clReadTMX-input.txt'):
-		SHead, Tail = os.path.split(SInputFileName)
-		SRoot, SExtension = os.path.splitext(Tail)
-		SRootOut = SRoot + 's01' + SExtension
-		SFileNameOut = os.path.join(SHead, SRootOut)
-		
-		print ("%(SHead)s %(Tail)s %(SRoot)s %(SExtension)s %(SFileNameOut)s\n" % locals())
-		return
 	
 	def tmx2tree(self, STmxIn):
 		root = ET.fromstring(STmxIn)
 		return
 
+
 if __name__ == '__main__':
+	'''
+	running script if the module is called from the main
+	'''
 	STmxIn = pathlib.Path(sys.argv[1]).read_text()
+	OGenerateOutputFileNames = clGenerateOutputFileNames(sys.argv[1], ['uk-UA', 'en-GB'])
 	OReadTMX = clReadTMX(STmxIn)
+	
 	pass
+
 
