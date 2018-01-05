f6ec1867bd3bd494b47492a9eda2bbcefc62438f
Bogdan Babych <bogdan.babych@gmail.com>
2018-01-05 00:48:38 +0000

    s006 implementation of reading string class

--- src/p1020k1010readText/k1010m1010readText.py
+++ src/p1020k1010readText/k1010m1010readText.py
@@ -3,15 +3,43 @@ Created on 18 Dec 2017
 
 @author: bogdan
 '''
+import sys, os, re
+import pathlib
 
-class MyClass(object):
+
+
+class clFile2Str(object):
     '''
-    classdocs
+    this class converts a file into a string for a more systematic processing 
+    '''
+    def __init__(self, SFileIn=sys.argv[1]):
+        self.SContent = ''
+        try:
+            self.SContent = pathlib.Path(SFileIn).read_text()
+        except:
+            pass
+        
+    def getData(self):
+        return self.SContent
+    
+    def printData(self):
+        print(self.SContent)
+        return
+
+
+class clReadTMX(object):
+    '''
+    readsTMX file / string and outputs one-line per-sentence output 
     '''
 
 
-    def __init__(self, params):
+    def __init__(self):
         '''
         Constructor
         '''
         
+
+if __name__ == '__main__':
+    OFile2Str = clFile2Str(sys.argv[1])
+    OFile2Str.printData()
+    
--- src/p1020k1010readText/k1010m1010readText.sh
+++ src/p1020k1010readText/k1010m1010readText.sh
@@ -1,1 +1,2 @@
-python p1020k1010m1010readText.py 
+# creating as many output files, as there are names of languages in tmx file, using a prefix and stage increment, use current directory -- with the destination folder being part of the prefix
+python p1020k1010m1010readText.py ../../../p1020d/k1010s010-TM_ENG-UKR.tmx >../../../p1020d/k1010s010-TM_ENG-UKR-debug.txt
