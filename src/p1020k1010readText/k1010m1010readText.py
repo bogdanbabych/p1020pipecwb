35acf531f71e56fc9b374018f5808a38e9e624a0
Bogdan Babych <bogdan.babych@gmail.com>
2018-01-05 12:26:25 +0000

    s000x development update

--- src/p1020k1010readText/k1010m1010readText.py
+++ src/p1020k1010readText/k1010m1010readText.py
@@ -5,24 +5,34 @@ Created on 18 Dec 2017
 '''
 import sys, os, re
 import pathlib
+import xml.etree.ElementTree as ET
 
 
 class clReadTMX(object):
-    '''
-    readsTMX file / string and outputs one-line per-sentence output 
-    '''
+	'''
+	readsTMX file / string and outputs one-line per-sentence output 
+	'''
+	def __init__(self, STmxIn):
+		'''
+		Constructor
+		'''
 
-
-    def __init__(self, STmxIn):
-        '''
-        Constructor
-        '''
-        
-        print("output\n", STmxIn)
-        
+		# print("output\n", STmxIn)
+		self.getFileOut(sys.argv[1])
+		return
+	
+	def getFileOut(self, SInputFileName = './clReadTMX-input.txt'):
+		SHead, Tail = os.path.split(SInputFileName)
+		SRoot, SExtension = os.path.splitext(Tail)
+		print ("%(SHead)s %(Tail)s %(SRoot)s %(SExtension)s\n")
+		return
+	
+	def tmx2tree(self, STmxIn):
+		root = ET.fromstring(STmxIn)
+		return
 
 if __name__ == '__main__':
-    STmxIn = pathlib.Path(sys.argv[1]).read_text()
-    OReadTMX = clReadTMX(STmxIn)
-    pass
-    
+	STmxIn = pathlib.Path(sys.argv[1]).read_text()
+	OReadTMX = clReadTMX(STmxIn)
+	pass
+
