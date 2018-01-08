62bbb03c730638aa1fb40f09a401d38d13a20188
Bogdan Babych <bogdan.babych@gmail.com>
2018-01-08 16:33:11 +0000

    s000x development update

--- src/p1020k1010readText/k1010m1010readText.py
+++ src/p1020k1010readText/k1010m1010readText.py
@@ -41,6 +41,9 @@ class clReadTMX(object):
 			for xmlTUV in xmlTU.findall('tuv'):
 				# testing with print 
 				print('\t', xmlTUV.attrib)
+				for el in xmlTUV.itertext():
+					print('\t\tITERTEXT', str(el))
+					
 				# print(xmlTUV.text, '\n')
 				pass
 				
