7a4e7f22ddc842e9fe043611cd3040548a352c95
Bogdan Babych <bogdan.babych@gmail.com>
2018-03-26 17:20:08 +0100

    s000x development update

--- src/p1020k1030matchTerms/k1030m1010matchTerms.py
+++ src/p1020k1030matchTerms/k1030m1010matchTerms.py
@@ -61,7 +61,8 @@ class clMatchTerms(object):
 				# re.sub(CRETerms, <term>\0</term>, STextIn)
 				for match in re.finditer(CRETerms, SSource):
 					SSource = re.sub(match.group(0), '<term>\g<0></term>', SSource)
-					# print(match.group(0))
+					print(match.group(0))
+				print('')
 				# print(SSource + '\t', STarget)
 					
 				
