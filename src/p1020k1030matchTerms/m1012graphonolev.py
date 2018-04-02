a41acf77a7d502259681663d9f01e4b68ea77319
Bogdan Babych <bogdan.babych@gmail.com>
2018-04-02 17:54:28 +0100

    s001x April 2018 development update

--- src/p1020k1030matchTerms/m1012graphonolev.py
+++ src/p1020k1030matchTerms/m1012graphonolev.py
@@ -242,7 +242,7 @@ class clGraphonolev(object):
 
 		
 		# return (Levenshtein0, Levenshtein1, Levenshtein0Norm, Levenshtein1Norm)
-		return (Levenshtein0, Levenshtein1, Levenshtein0Norm, Levenshtein1Norm, LevenshteinI2, LevenshteinI2Norm, LevenshteinI4, LevenshteinI4Norm, LevenshteinI6, LevenshteinI6Norm, LevenshteinI8, LevenshteinI8Norm)
+		return (Levenshtein0, evenshtein0Norm, LevenshteinI2, LevenshteinI2Norm, LevenshteinI4, LevenshteinI4Norm, LevenshteinI6, LevenshteinI6Norm, LevenshteinI8, LevenshteinI8Norm, Levenshtein1, LLevenshtein1Norm)
 
 
 	def printMatrix(self, m):
@@ -293,8 +293,8 @@ if __name__ == '__main__':
 		# LGraphFeat1 = OGraphonolev.str2Features(SW1, SLangID1)
 		# FDebug.write('SW2 = %(SW2)s; SLangID2 = %(SLangID2)s\n' % locals())
 		# LGraphFeat2 = OGraphonolev.str2Features(SW2, SLangID2)
-		(Lev0, Lev1, Lev0Norm, Lev1Norm, LevenshteinI2, LevenshteinI2Norm, LevenshteinI4, LevenshteinI4Norm, LevenshteinI6, LevenshteinI6Norm, LevenshteinI8, LevenshteinI8Norm) = OGraphonolev.computeLevenshtein(SW1, SW2, SLangID1, SLangID2)
-		sys.stdout.write('%(SW1)s, %(SW2)s, %(Lev0)d, %(Lev1).4f, %(Lev0Norm).4f, %(Lev1Norm).4f, %(LevenshteinI2).4f, %(LevenshteinI2Norm).4f, %(LevenshteinI4).4f, %(LevenshteinI4Norm).4f, %(LevenshteinI6).4f, %(LevenshteinI6Norm).4f, %(LevenshteinI8).4f, %(LevenshteinI8Norm).4f\n' % locals())
+		(Lev0, Lev0Norm, LevenshteinI2, LevenshteinI2Norm, LevenshteinI4, LevenshteinI4Norm, LevenshteinI6, LevenshteinI6Norm, LevenshteinI8, LevenshteinI8Norm, Lev1, Lev1Norm) = OGraphonolev.computeLevenshtein(SW1, SW2, SLangID1, SLangID2)
+		sys.stdout.write('%(SW1)s, %(SW2)s, %(Lev0)d, %(Lev0Norm).4f, %(LevenshteinI2).4f, %(LevenshteinI2Norm).4f, %(LevenshteinI4).4f, %(LevenshteinI4Norm).4f, %(LevenshteinI6).4f, %(LevenshteinI6Norm).4f, %(LevenshteinI8).4f, %(LevenshteinI8Norm).4f, %(Lev1).4f, %(Lev1Norm).4f\n' % locals())
 			
 		
 		
